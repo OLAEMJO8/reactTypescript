9b98381e3869196899735dd324129ae710886fe4
Majo <olaecheamariajose@gmail.com>
2024-10-03 09:19:48 -0300

    Feat. Usuario - tipado estricto

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Counter } from "./components/Counter";
+import { Usuario } from "./components/Usuario";
 function App() {
   return (
     <div>
@@ -7,6 +8,8 @@ function App() {
       <hr />
 
       <Counter />
+      <hr />
+      <Usuario />
     </div>
   );
 }
---
+++ src/components/Usuario.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+interface User {
+  uid: string;
+  name: string;
+}
+// tempUser es de tipo User
+const tempUser: User = {
+  uid: "1234567890AS",
+  name: "Juan",
+};
+
+export const Usuario = () => {
+  const [user, setUser] = useState<User>(tempUser);
+
+  const login = () => {
+    setUser({
+      uid: "ABC123456",
+      name: "Majo",
+    });
+  };
+
+  return (
+    <div>
+      <h2>Usuario: useState</h2>
+
+      <button onClick={login} className="btn btn-outline-primary mt-2  mx-2">
+        Login
+      </button>
+      <button
+        onClick={() => setUser(tempUser)}
+        className="btn btn-outline-primary mt-2  mx-2"
+      >
+        Cambiar de usuario
+      </button>
+      <br />
+      <br />
+      {!user ? <pre> No hay usuario</pre> : <pre>{JSON.stringify(user)}</pre>}
+    </div>
+  );
+};
